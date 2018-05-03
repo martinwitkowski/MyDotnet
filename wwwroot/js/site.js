199d37adf51f80de814a538e05310ea7fcfc1227
Marty Witkowski <martywit96@gmail.com>
2018-05-02 22:33:51 -0400

    Init

---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Write your JavaScript code.
+function ThisFunction(id, person) {
+    
+}
+
+
+console.log('Hello World');
+
+//This is my name
+
+//This is my name
+
+function helloSir(){
+    return "Martin";
+}
